88adc66b61a7247c39030e704fd4cd5e60647615
Veselchun <veselina.krusteva@gmail.com>
2022-04-24 20:45:44 +0300

    End of section 13

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -59,11 +60,29 @@ namespace API.Data
                 .SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users 
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+            var query = _context.Users.AsQueryable();
+
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+            //C# 8 new switch synax
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+
+            var result = query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                            .AsNoTracking();
+
+            return await PagedList<MemberDto>.CreateAsync(result, userParams.PageNumber, userParams.PageSize);
         }
     }
 }
--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -4,9 +4,16 @@ namespace API.Extensions
 {
     public static class ClaimsPrincipleExtensions
     {
+        //ClaimTypes.Name => JwtRegisteredClaimNames.UniqueName
+        //ClaimTypes.NameIdentifier => JwtRegisteredClaimNames.NameId
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value; ////takes the value from the token
+            return user.FindFirst(ClaimTypes.Name)?.Value; ////takes the value from the token
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value); ////takes the value from the token
         }
     }
 }
